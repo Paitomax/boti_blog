4814fb8e8b19865895803c9761098e9110da4142
Guilherme Nascimento <paitomax@gmail.com>
2020-06-03 20:51:30 -0300

    receive repository as parameter

--- lib/src/sign_in/sign_in_bloc.dart
+++ lib/src/sign_in/sign_in_bloc.dart
@@ -1,8 +1,13 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
+import 'package:botiblog/src/sign_in/sign_in_repository_interface.dart';
 import './bloc.dart';
 
 class SignInBloc extends Bloc<SignInEvent, SignInState> {
+  final SignInRepositoryInterface signInRepository;
+
+  SignInBloc(this.signInRepository);
+
   @override
   SignInState get initialState => SignInInitial();
 
