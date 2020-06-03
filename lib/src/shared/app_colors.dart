1353fc66f205de983217f1eea253113a8b2d107b
Guilherme Nascimento <paitomax@gmail.com>
2020-06-03 15:55:55 -0300

    added AppColors

---
+++ lib/src/shared/app_colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppColors {
+  static final Color black = Color(0xFF0D0D0D);
+  static final Color white = Color(0xFFFBFCFA);
+  static final Color orange = Color(0xFFF25D27);
+  static final Color lightOrange = Color(0xFFF28322);
+  static final Color green = Color(0xFFBAD62A);
+  static final Color yellow = Color(0xFFD0DD25);
+  static final Color blue = Color(0xFF5BB5D9);
+}
