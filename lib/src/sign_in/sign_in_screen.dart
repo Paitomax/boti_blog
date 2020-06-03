41fc1f561749a5b4513ddb569f4d34b5b2c4c6d2
Guilherme Nascimento <paitomax@gmail.com>
2020-06-03 18:35:15 -0300

    SignInScreen initial commit

---
+++ lib/src/sign_in/sign_in_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class SignInScreen extends StatefulWidget {
+  static final routeName = '/sign_in';
+
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
