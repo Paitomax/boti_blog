b4be33009ee7a704a854b8111344092c6d086364
Guilherme Nascimento <paitomax@gmail.com>
2020-06-03 21:29:25 -0300

    requestLogin receive user/pass args

--- lib/src/sign_in/sign_in_event.dart
+++ lib/src/sign_in/sign_in_event.dart
@@ -2,4 +2,9 @@ abstract class SignInEvent {
   const SignInEvent();
 }
 
-class SignInRequested extends SignInEvent {}
+class SignInRequested extends SignInEvent {
+  final String email;
+  final String password;
+
+  SignInRequested(this.email, this.password);
+}
