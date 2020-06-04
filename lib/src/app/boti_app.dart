1787841643fbf8a598c2243dad98a4cbff4fdbba
Guilherme Nascimento <paitomax@gmail.com>
2020-06-03 21:08:07 -0300

    added bloc/repository to provider

--- lib/src/app/boti_app.dart
+++ lib/src/app/boti_app.dart
@@ -1,8 +1,11 @@
 import 'package:botiblog/src/app/app_routes.dart';
-import 'package:botiblog/src/shared/theme/app_colors.dart';
+import 'package:botiblog/src/sign_in/sign_in_bloc.dart';
+import 'package:botiblog/src/sign_in/sign_in_repository.dart';
+import 'package:botiblog/src/sign_in/sign_in_repository_interface.dart';
 import 'package:botiblog/src/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BotiApp extends StatelessWidget {
   @override
@@ -14,6 +17,28 @@ class BotiApp extends StatelessWidget {
         ),
         initialRoute: SplashScreen.routeName,
         routes: AppRoutes.routes);
-    return materialApp;
+    return _providers(
+      child: materialApp,
+    );
+  }
+
+  MultiRepositoryProvider _providers({Widget child}) {
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<SignInRepositoryInterface>(
+          create: (context) => SignInRepository(),
+        ),
+      ],
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => SignInBloc(
+              RepositoryProvider.of<SignInRepositoryInterface>(context),
+            ),
+          ),
+        ],
+        child: child,
+      ),
+    );
   }
 }
