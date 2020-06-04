d0243eab0350a9c53f3f806872eaf432d4d2273c
Guilherme Nascimento <paitomax@gmail.com>
2020-06-03 22:32:20 -0300

    signup initial commit

---
+++ lib/src/sign_up/sign_up_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatefulWidget {
+  static final String routeName = '/signup';
+
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
