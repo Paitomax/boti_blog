c31593ed32ae238da81c47555bdf5dca005d8663
Guilherme Nascimento <paitomax@gmail.com>
2020-06-04 01:09:00 -0300

    home screen initial commit

---
+++ lib/src/home/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  static final String routeName = '/home';
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Estou na home galereeee, até que enfim'),
+      ),
+    );
+  }
+}
