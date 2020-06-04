3a8d065edcaa32b006885cfc3789e40acbf1023f
Guilherme Nascimento <paitomax@gmail.com>
2020-06-04 01:09:17 -0300

    added home to routes

--- lib/src/app/app_routes.dart
+++ lib/src/app/app_routes.dart
@@ -1,3 +1,4 @@
+import 'package:botiblog/src/home/home_screen.dart';
 import 'package:botiblog/src/sign_in/sign_in_screen.dart';
 import 'package:botiblog/src/sign_up/sign_up_screen.dart';
 import 'package:botiblog/src/splash/splash_screen.dart';
@@ -7,5 +8,6 @@ class AppRoutes {
     SplashScreen.routeName: (context) => SplashScreen(),
     SignInScreen.routeName: (context) => SignInScreen(),
     SignUpScreen.routeName: (context) => SignUpScreen(),
+    HomeScreen.routeName: (context) => HomeScreen(),
   };
 }
