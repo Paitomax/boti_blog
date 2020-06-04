e0cb0e7e83c4e589c23c136b8af7662368a7197e
Guilherme Nascimento <paitomax@gmail.com>
2020-06-04 12:54:05 -0300

    initial commit

---
+++ lib/src/home/user_news/model/user_post_response_model.dart
@@ -0,0 +1,11 @@
+class UserPostResponseModel {
+  final int id;
+  final String date;
+  final String text;
+  final int userId;
+  final String userName;
+  final String userEmail;
+
+  UserPostResponseModel(this.id, this.date, this.text, this.userId, this.userName, this.userEmail);
+
+}
