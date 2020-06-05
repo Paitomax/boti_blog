55a6ad6e6d77d953c87a4f108d2cc32a3066b3e5
Guilherme Nascimento <paitomax@gmail.com>
2020-06-04 23:47:37 -0300

    added dio

--- lib/src/home/boti_news/boti_news_data_provider.dart
+++ lib/src/home/boti_news/boti_news_data_provider.dart
@@ -1,6 +1,11 @@
 import 'package:botiblog/src/home/boti_news/model/boti_news_response_model.dart';
+import 'package:dio/dio.dart';
 
 class BotiNewsDataProvider {
+  final Dio dio;
+
+  BotiNewsDataProvider(this.dio);
+
   Future<BotiNewsResponseModel> fetch() async {
     return BotiNewsResponseModel(null);
   }
