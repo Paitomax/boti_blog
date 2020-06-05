8644fc4d03d68b772407f0801aafe39308081f87
Guilherme Nascimento <paitomax@gmail.com>
2020-06-05 15:44:53 -0300

    added MockSignUpRepository to mockito

--- test/mockito.dart
+++ test/mockito.dart
@@ -4,8 +4,10 @@ import 'package:botiblog/src/shared/auth/auth_event.dart';
 import 'package:botiblog/src/shared/auth/auth_state.dart';
 import 'package:botiblog/src/shared/user/user_repository_interface.dart';
 import 'package:botiblog/src/sign_in/sign_in_repository_interface.dart';
+import 'package:botiblog/src/sign_up/sign_up_repository_interface.dart';
 import 'package:mockito/mockito.dart';
 
 class MockAuthBloc extends MockBloc<AuthEvent, AuthState> implements AuthBloc {}
 class MockUserRepository extends Mock implements UserRepositoryInterface {}
 class MockSignInRepository extends Mock implements SignInRepositoryInterface {}
+class MockSignUpRepository extends Mock implements SignUpRepositoryInterface {}
