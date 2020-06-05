574ecded7136c731c024335df1a8d8695c37743a
Guilherme Nascimento <paitomax@gmail.com>
2020-06-05 17:28:06 -0300

    Models extends equatable

--- lib/src/home/boti_news/model/boti_news_user_model.dart
+++ lib/src/home/boti_news/model/boti_news_user_model.dart
@@ -1,9 +1,10 @@
+import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'boti_news_user_model.g.dart';
 
 @JsonSerializable(nullable: false, anyMap: true)
-class BotiNewsUserModel {
+class BotiNewsUserModel extends Equatable {
   @JsonKey(name: 'name')
   final String name;
   @JsonKey(name: 'profile_picture')
@@ -20,4 +21,7 @@ class BotiNewsUserModel {
   String toString() {
     return 'BotiNewsUserModel{name: $name, profilePictureUrl: $profilePictureUrl}';
   }
+
+  @override
+  List<Object> get props => [name, profilePictureUrl];
 }
