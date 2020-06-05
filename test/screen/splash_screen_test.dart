a2133cac142244ef36dc417c9472e319a3a59f15
Guilherme Nascimento <paitomax@gmail.com>
2020-06-05 20:33:55 -0300

    SplashScreen unit test

---
+++ test/screen/splash_screen_test.dart
@@ -0,0 +1,23 @@
+import 'package:botiblog/src/splash/splash_screen.dart';
+import 'package:botiblog/src/splash/splash_screen_texts.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('SplashScreen', () {
+    Widget _buildMainApp() {
+      return MaterialApp(
+        home: SplashScreen(),
+      );
+    }
+
+    testWidgets('Should render SplashScreen', (WidgetTester tester) async {
+      final app = _buildMainApp();
+      await tester.pumpWidget(app);
+
+      expect(find.text(SplashScreenTexts.email), findsOneWidget);
+      expect(find.text(SplashScreenTexts.name), findsOneWidget);
+      expect(find.byIcon(Icons.flash_on), findsOneWidget);
+    });
+  });
+}
