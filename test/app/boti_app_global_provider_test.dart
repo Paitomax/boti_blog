77bc49c3bf282f1778787f8fd992962505237b10
Guilherme Nascimento <paitomax@gmail.com>
2020-06-05 13:38:53 -0300

    BotiAppGlobalProvider unit test

---
+++ test/app/boti_app_global_provider_test.dart
@@ -0,0 +1,21 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:botiblog/src/app/boti_app.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Should render child', (WidgetTester tester) async {
+    final child = MaterialApp(home: Text('Oi'));
+
+    await tester.pumpWidget(BotiAppGlobalProvider(child: child));
+
+    expect(find.byWidget(child), findsOneWidget);
+    expect(find.text('Oi'), findsOneWidget);
+  });
+}
